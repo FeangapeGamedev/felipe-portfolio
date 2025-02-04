6a910260ba38f6b81d98e16c2172135e8ef9f12b
Felipe Garcia <felipe_garciapereira@trimble.com>
2025-02-05 11:38:58 +1300

    Fixes weird collider issues

--- src/components/Walls.jsx
+++ src/components/Walls.jsx
@@ -49,25 +49,27 @@ export const Walls = ({ wallMaterial }) => {
         </mesh>
       </RigidBody>
 
-      {/* Right Wall (Collider Only) - Tilted */}
+      {/* Right Wall (Tilted) */}
       <RigidBody type="fixed" colliders="cuboid">
         <mesh
           position={[floorSize / 2, wallHeight / 2, 0]}
-          rotation={[0, -Math.PI / 2, -tiltAngle]} // Rotate + inverse tilt
+          rotation={[0, Math.PI / 2, tiltAngle]} // Rotate + tilt
+          raycast={(...args) => THREE.Mesh.prototype.raycast.apply(this, args)}
         >
-          <boxGeometry args={[wallThickness, wallHeight, floorSize]} />
-          <meshStandardMaterial visible={false} /> {/* Invisible Wall */}
+          <boxGeometry args={[floorSize, wallHeight, wallThickness]} />
+          <primitive attach="material" object={wallMaterialFromTextures} visible={false} />
         </mesh>
       </RigidBody>
 
-      {/* Front Wall (Collider Only) - Tilted */}
+      {/* Front Wall (Tilted) */}
       <RigidBody type="fixed" colliders="cuboid">
         <mesh
           position={[0, wallHeight / 2, floorSize / 2]}
-          rotation={[0, 0, -tiltAngle]} // Opposite tilt
+          rotation={[0, 0, -tiltAngle]} // Apply tilt on Z-axis
+          raycast={(...args) => THREE.Mesh.prototype.raycast.apply(this, args)}
         >
           <boxGeometry args={[floorSize, wallHeight, wallThickness]} />
-          <meshStandardMaterial visible={false} /> {/* Invisible Wall */}
+          <primitive attach="material" object={wallMaterialFromTextures} visible={false}/>
         </mesh>
       </RigidBody>
     </group>
