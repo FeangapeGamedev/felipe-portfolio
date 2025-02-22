d7832f9bf3e9f6dd1c3956770a0fa26c97b04abd
Felipe Garcia <felipe_garciapereira@trimble.com>
2025-02-22 23:34:46 +1300

    Added Scifi Door, fixed colldiers

--- src/data/roomData.js
+++ src/data/roomData.js
@@ -46,7 +46,7 @@ export const roomData = [
     name: "Project Room",
     position: [0, 0, 0],
     spawnPositionForward: [-4, 0, 6],
-    spawnPositionBackward: [0, 0, -4],
+    spawnPositionBackward: [4.5, 0, -5.5],
     width: 15,
     depth: 15,
     height: 5,
@@ -74,12 +74,12 @@ export const roomData = [
       {
         id: "door2",
         type: "door",
-        position: [-1, 1.5, -7.2], // Position the door at the back wall
-        rotation: [0, -55, 0], // Rotate the door to face the room
+        position: [4, 0, -7.3], // Position the door at the back wall
+        rotation: [0, Math.PI, 0], // Rotate the door to face the room
         direction: "forward",
         label: "Go to Game Room",
-        model: "/src/assets/3dModels/door/scene.gltf", // No model path, will use fallback
-        scale: [0.14, 0.12, 0.14], // Default scale
+        model: "src/assets/3dModels/SciFiDoor.glb", // No model path, will use fallback
+        scale: [0.9, 0.9, 0.9], // Default scale
       },
       {
         id: "door3",
@@ -88,18 +88,18 @@ export const roomData = [
         rotation: [0, -55, 0], // Rotate the door to face the room
         direction: "backward",
         label: "Go to Introduction Room",
-        model: "/src/assets/3dModels/door/scene.gltf", // No model path, will use fallback
+        model: "/src/assets/3dModels/door/scene.gltf",
         scale: [0.14, 0.12, 0.14], // Default scale
       },
       {
         id: 1,
         type: "project",
-        position: [-5, 0, -2], // Position the project on the left side
-        rotation: [0, -55, 0], // Rotate the project to face the room
+        position: [-5, 0, -3], // Position the project on the left side
+        rotation: [0, 0, 0], // Rotate the project to face the room
         label: "Project One",
         color: "blue",
         model: "/src/assets/3dModels/Construction.glb", // No model path, will use fallback
-        scale: [0.7, 0.7, 0.7], // Default scale
+        scale: [0.65, 0.65, 0.65], // Default scale
       },
     ],
   },
@@ -107,7 +107,7 @@ export const roomData = [
     id: 3,
     name: "Game Room",
     position: [0, 0, 0],
-    spawnPositionForward: [0, 0, 4],
+    spawnPositionForward: [5.5, 0, 6],
     spawnPositionBackward: [0, 0, -4],
     width: 15,
     depth: 15,
@@ -136,12 +136,12 @@ export const roomData = [
       {
         id: "door4",
         type: "door",
-        position: [1, 1.5, 7.2], // Position the door at the front wall
-        rotation: [0, -55, 0], // Rotate the door to face the room
+        position: [5, 0, 7.7], // Position the door at the front wall
+        rotation: [0, 0, 0], // Rotate the door to face the room
         direction: "backward",
         label: "Go to Project Room",
-        model: "/src/assets/3dModels/door/scene.gltf", // No model path, will use fallback
-        scale: [0.14, 0.12, 0.14], // Default scale
+        model: "src/assets/3dModels/SciFiDoor.glb", // No model path, will use fallback
+        scale: [0.9, 0.9, 0.9], // Default scale
       },
     ],
   },
